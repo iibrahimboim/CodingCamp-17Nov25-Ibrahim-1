83e810432653262ea7bcb5a0188a8a2677268af5
iibrahimboim <ytjazs04@gmail.com>
2025-11-21 00:30:01 +0700

    Mini Project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+welcomeMessege();
+
+function welcomeMessege() {
+    let userName = prompt("Please enter your name:");
+
+    if (userName === '' || userName === null) {
+        useName("Guest");
+    }
+
+    document.getElementById("welcome-speech").innerHTML = "Welcome " + userName + "!";
+}
